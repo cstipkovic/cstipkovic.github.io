6cbd16e8ff86180672d29cb97051601e7a50cd2e
Clauber Stipkovic <clauber.halic@gmail.com>
2016-09-20 01:01:03 -0300

    tmp intv

--- modal-intv/intv-modal-ad.js
+++ modal-intv/intv-modal-ad.js
@@ -3,37 +3,34 @@
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-268918-3', 'auto');
-ga('send', 'event', 'Pageview', 'access', 'Campanha One World');
+ga('create', 'UA-84499702-1', 'auto');
+ga('send', 'event', 'Pageview', 'access', 'AER_GIG_ONEWORLD');
 
 (function(d, w) {
-	function setCloseButton() {
-		var closeButton = d.createElement('span');
-
-		closeButton.classList.add('close');
-		closeButton.setAttribute('style', 'color: #aaa; float: right; font-size: 28px; font-weight: bold;');
-		closeButton.innerHTML = 'x';
-
-		return closeButton;
-	}
-
 	function setVideoAd() {
+		var contentDiv = d.createElement('div');
 		var videoTag = d.createElement('video');
+
+		contentDiv.setAttribute('style', 'border:none; width:100%;height:100%;')
 
 		videoTag.id = 'video-intv-ad';
 		// videoTag.setAttribute('controls', true);
-		videoTag.setAttribute('src', 'http://www.w3schools.com/tags/movie.mp4');
+		videoTag.setAttribute('src', 'amarrotados.mp4');
 		videoTag.setAttribute('type', 'video/mp4');
-		videoTag.setAttribute('poster', 'poster.png');
+		videoTag.setAttribute('poster', 'latam-bg.jpg');
 
-		return videoTag;
+		videoTag.setAttribute('style', 'max-width: 968px');
+
+		contentDiv.appendChild(videoTag);
+
+		return contentDiv;
 	}
 
 	function setContentModal() {
 		var contentModal = d.createElement('div');
 
 		contentModal.classList.add('modal-content');
-		contentModal.setAttribute('style', 'margin: 15% auto; width: 320px;');
+		contentModal.setAttribute('style', 'margin: 15% auto; max-height: auto;max-width: 968px;');
 
 		// contentModal.appendChild(setCloseButton());
 		contentModal.appendChild(setVideoAd());
@@ -63,14 +60,14 @@ ga('send', 'event', 'Pageview', 'access', 'Campanha One World');
 
 	function closeModal() {
 		d.getElementById('overlay-intv').style.display = "none";
-		ga('send', 'event', 'Video', 'ended', 'Campanha One World');
+		ga('send', 'event', 'Video', 'VideoCompletion', 'AER_GIG_ONEWORLD');
 	}
 
 	function clickToPlay() {
 		var video = d.getElementById('video-intv-ad');
 
 		video.play();
-		ga('send', 'event', 'Video', 'play', 'Campanha One World');
+		ga('send', 'event', 'Video', 'ClickPlay', 'AER_GIG_ONEWORLD');
 	}
 
 	d.body.appendChild(buildModal());
