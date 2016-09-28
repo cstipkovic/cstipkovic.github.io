37ad88e7d655125fd7d8876a69a333291fe9a988
Clauber Stipkovic <cin_chalic@uolinc.com>
2016-09-28 18:29:01 -0300

    Updating GA, and trying to remove jquery

--- js/main.js
+++ js/main.js
@@ -1,34 +1,45 @@
-function setCurrentlyAge () {
-    var date = new Date();
+(function(d, w) {
+	function setCurrentlyAge() {
+		var date = new Date();
+		var elemCurrentAge = d.getElementsByClassName('currentlyAge')[0];
 
-    $('.currentlyAge').text(date.getFullYear() - 1984);
-}
+		elemCurrentAge.innerHTML = (date.getFullYear() - 1984);
+	}
 
-function setLanguageOnLoad (browserLanguage) {
-    if (browserLanguage === 'pt-BR') {
-        $('#portuguese').show();
-    } else {
-        $('#english').show();
-    }
-}
+	function setLanguageOnLoad(browserLanguage) {
+		if (browserLanguage === 'pt-BR') {
+			$('#portuguese').show();
+		} else {
+			$('#english').show();
+		}
+	}
 
-function changeLanguage () {
-    $('#menu a').click(function () {
-        $('#menu a').removeClass('currentlyLanguage');
-        $(this).addClass('currentlyLanguage');
-        
-        if ($(this).attr('data-lang') === 'pt-BR') {
-            $('#english').hide();
-            $('#portuguese').show();
-        } else {
-            $('#portuguese').hide();
-            $('#english').show();
-        }
-    });
-}
+	function changeLanguage() {
+		$('#menu a').click(function() {
+			$('#menu a').removeClass('currentlyLanguage');
+			$(this).addClass('currentlyLanguage');
 
-$(document).ready(function () {
-    setCurrentlyAge();
-    setLanguageOnLoad();
-    changeLanguage();
-});
+			if ($(this).attr('data-lang') === 'pt-BR') {
+				$('#english').hide();
+				$('#portuguese').show();
+			} else {
+				$('#portuguese').hide();
+				$('#english').show();
+			}
+		});
+	}
+
+	function linkMozillaReps() {
+		ga('send', 'event', 'LinkClick', 'link-mozilla-reps', 'cs_github_io');
+	}
+
+	function _init() {
+		setCurrentlyAge();
+		setLanguageOnLoad();
+		changeLanguage();
+
+		d.getElementsByClassName('link-mozilla-reps')[0].addEventListner('click', linkMozillaReps, false);
+	}
+
+	w.load(_init());
+})(document, window);
