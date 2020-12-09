1a81e5c6937b9f3aa81bb3b8511d21bbe326c58c
Clauber Stipkovic <clauber.halic@gmail.com>
2020-12-09 10:37:28 -0300

    Adding next js for start build the blog

---
+++ blog/pages/404.js
@@ -0,0 +1,7 @@
+const Custom404 = () => {
+    return (
+        <h1>404 - Page Not Found!</h1>
+    )
+};
+
+export default Custom404;
---
+++ blog/pages/index.js
@@ -0,0 +1,5 @@
+const Blog = () => {
+    return <div>Welcome to this blog, using Next.js</div>
+};
+
+export default Blog
