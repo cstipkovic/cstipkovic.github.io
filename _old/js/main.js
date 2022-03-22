4558a113e6342de7f174ade2376b0b51a2ddb632
Clauber Stipkovic <clauber.halic@gmail.com>
2022-03-21 22:35:50 -0300

    Moving old index webpage to new react

---
+++ _old/js/main.js
@@ -0,0 +1,47 @@
+'use strict';
+
+(function(d, w) {
+	function setCurrentlyAge() {
+		var date = new Date();
+		var elemCurrentAge = d.getElementsByClassName('currentlyAge')[0];
+
+		elemCurrentAge.innerHTML = (date.getFullYear() - 1984);
+	}
+
+	function setLanguageOnLoad(browserLanguage) {
+		if (browserLanguage === 'pt-BR') {
+			$('#portuguese').show();
+		} else {
+			$('#english').show();
+		}
+	}
+
+	function changeLanguage() {
+		$('#menu a').click(function() {
+			$('#menu a').removeClass('currentlyLanguage');
+			$(this).addClass('currentlyLanguage');
+
+			if ($(this).attr('data-lang') === 'pt-BR') {
+				$('#english').hide();
+				$('#portuguese').show();
+			} else {
+				$('#portuguese').hide();
+				$('#english').show();
+			}
+		});
+	}
+
+	function linkMozillaReps() {
+		ga('send', 'event', 'LinkClick', 'link-mozilla-reps', 'cs_github_io');
+	}
+
+	function _init() {
+		setCurrentlyAge();
+		setLanguageOnLoad();
+		changeLanguage();
+
+		d.getElementsByClassName('link-mozilla-reps')[0].addEventListener('click', linkMozillaReps, false);
+	}
+
+	w.onload = _init;
+})(document, window);
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* <div className="main-container">
+        <div className="main wrapper clearfix"> */}
+      <Main />
+      {/* </div>
+      </div> */}
+      <Footer />
+    </>
+  );
+}
+
+export default App;
